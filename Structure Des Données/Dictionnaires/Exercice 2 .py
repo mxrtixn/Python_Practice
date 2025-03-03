e28fb94e99cceb1703470551995c34448dacef55
mxrtixn <hamzaelma96@gmail.com>
2025-03-03 13:45:39 +0000

    Practice

---
+++ Structure Des Données/Dictionnaires/Exercice 2 .py
@@ -0,0 +1,14 @@
+informations_personnelles = {
+    "nom": "Hamza",
+    "prenom": "ELma",
+    "age": 17,
+    "ville": "Beni Mellal"
+}
+
+informations_personnelles["email"] = "elmanouzi.ha@gmail.com"
+
+informations_personnelles["age"] = 18
+
+del informations_personnelles["ville"]
+
+print(informations_personnelles)
